8bf43bf9518e7a386c9e13c79dd2be1d19665c5e
Jung Jaeyoung <jyihaan4@jbnu.ac.kr>
2022-12-20 03:41:00 +0900

    #11 최종

--- blogapp/models.py
+++ blogapp/models.py
@@ -6,7 +6,7 @@ from markdownx.models import MarkdownxField
 from markdownx.utils import markdown
 import os
 
-from uuslug import uuslug
+# from uuslug import uuslug
 
 
 class Tag(models.Model):
@@ -18,24 +18,6 @@ class Tag(models.Model):
 
     def get_absolute_url(self):
         return f'/blogtip/tag/{self.slug}/'
-
-
-# class Category(models.Model):
-#     name = models.CharField(max_length=50, unique=True)
-#     slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
-#
-#     def __str__(self):
-#         return self.name
-#
-#     def get_absolute_url(self):
-#         return f'/blogtip/category/{self.slug}/'
-#
-#     class Meta:
-#         verbose_name_plural = 'categories'
-#
-#     def save(self, *args, **kwargs):
-#         self.slug = uuslug(self.name, instance=self)
-#         super(Category, self).save(*args, **kwargs)
 
 class Post(models.Model):
     title = models.CharField(max_length=25)
